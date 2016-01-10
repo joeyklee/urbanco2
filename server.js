00c08bb4d3e9dc4b90e9e7fc49c4d0fa61fd23b2
Joey Lee <josephkanglee@gmail.com>
2016-01-10 13:53:34 -0800

    added views

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 
 
-app.get('/', function(req, res){
-	res.send('hello world');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/mobile', function (req, res) {
+    res.render('mobile');
+});
+
+app.get('/explore', function (req, res) {
+    res.render('explore');
+});
+
+
+// app.get('/', function(req, res){
+// 	res.send('hello world');
+// });
+
 app.use('/public', express.static('public'));
+app.use("/views/stylesheets",express.static("/views/stylesheets"));
 
 
 var port = Number(process.env.Port || 5000);
 app.listen(port);
+
+
+// https://www.youtube.com/watch?v=m5ribwPpIPw
