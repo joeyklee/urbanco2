e014a2c6b7409e223c0d57756b7bcb9f4a24e8e1
Joey Lee <josephkanglee@gmail.com>
2016-02-01 19:38:39 +0100

    added activate nav script -- needs to be debugged

---
+++ public/js/activatenav.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+// console.log($(".nav a"));
+// $(".nav a").on("click", function(){
+
+//    $(".nav a").find(".active").removeClass("active");
+//    $(this).parent().addClass("active");
+// });
+// })
+
