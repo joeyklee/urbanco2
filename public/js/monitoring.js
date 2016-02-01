749b30e238760abcb0e235e9d6f574ebf168f787
Joey Lee <josephkanglee@gmail.com>
2016-02-01 01:01:21 +0100

    added content to monitoring page

--- public/js/monitoring.js
+++ public/js/monitoring.js
@@ -34,12 +34,10 @@ var scrollVis = function() {
     // section is called.
     // var activateFunctions = [];
     activateFunctions = [
-            showTitle,
-            showHardware,
+    		showSensor,
             showResistanceApproach,
-            showGridData,
-            showExploreData,
-            showExploreData
+            show3dPlot,
+            showPreview
         ]
     // If a section has an update function
     // then it is called while scrolling
@@ -50,15 +48,11 @@ var scrollVis = function() {
 
     var chart = function(selection) {
         selection.each(function(rawData) {
-            // create svg and give it a width and height
-            // svg = d3.select(this).selectAll("svg").data([0, 1, 2, 3, 4, 5, 6, 7]);
             svg = d3.select(this).selectAll("svg").data(activateFunctions);
             svg.enter().append("svg").append("g");
 
             svg.attr("width", width + margin.left + margin.right);
             svg.attr("height", height + margin.top + margin.bottom);
-            // svg.attr("viewBox", "0 0 600 500").attr("preserveAspectRatio", "xMinYMin meet");
-
 
             // this group element will be used to contain all
             // other elements.
@@ -66,8 +60,6 @@ var scrollVis = function() {
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             setupVis();
-
-            // setupSections();
 
         });
     };
@@ -85,29 +77,19 @@ var scrollVis = function() {
 			.attr("class", "title openvis-title")
 			.attr("x", width / 2)
 			.attr("y", (height / 3) + (height / 5))
-			.text("hello");
+			.text("");
 		g.selectAll(".openvis-title")
 			.attr("opacity", 0);
 
+
 		// // add image
-		// var sensorimage = g.append("g")
-		// .attr("class", "sensorsImgs")
-		// .attr("opacity", 0);
-
-		// sensorimage.append("svg:image")
-		// 	.attr("width", "400")
-  //           .attr("height", "500")
-  //           .att("class", "sensorImg")
-  //           .attr("opacity", 1)
-  //           .attr("xlink:href", "public/images/sensorlineup.JPG");
-
-  		// add image
-		g.append("g").attr("class", "sensorsImg").attr("opacity", 0)
+		g.append("g").attr("class", "sensorAnnotated").attr("opacity", 0)
 		.append("image")
-			.attr("width", "400")
+			.attr("width", "450")
             .attr("height", "500")
+            .attr("margin-top", "50px")
             .attr("opacity", 1)
-            .attr("xlink:href", "public/images/sensorlineup.JPG");
+            .attr("xlink:href", "public/images/boxphoto-annotated-img.jpg");
 
 		queue()
 			.defer(d3.xml, "public/images/resistance-approach-2.svg")
@@ -131,65 +113,133 @@ var scrollVis = function() {
 
 		}
 
+		queue()
+			.defer(d3.xml, "public/images/3d-grid.svg")
+			.await(addgrid3d)
 
-        queue()
-        	.defer(d3.xml, "public/images/emissions-map.svg")
-        	.await(addMap)
+		var grid3d = g.append("g")
+			.attr("class", "grid3d")
+			.attr("opacity",0)
+			.attr("transform", "scale(0.8)");
 
-        var emissionsMap = g.append("g")
-        	.attr("class", "emissionsMap");
+		function addgrid3d(error, xml){
+			console.log(xml);
+			var items = Array.from(xml.getElementsByTagName("svg")[0].childNodes);
+			items.forEach(function(val) {
+			    grid3d.node().appendChild(val);
+			});
 
-        function addMap(error, xml){
-        	console.log(xml);
-        	var items = Array.from(xml.getElementsByTagName("svg")[0].childNodes);
-        	items.forEach(function(val) {
-        	    emissionsMap.node().appendChild(val);
-        	});
+		}
 
-        	g.selectAll(".emissionsMap")
-        		.attr("transform", "translate(100), scale(0.5)");
+		// // add image
+		g.append("g").attr("class", "previewimg").attr("opacity", 0)
+		.append("image")
+			.attr("width", "600")
+            .attr("height", "500")
+            .attr("opacity", 1)
+            .attr("xlink:href", "public/images/preview.png");
 
-        	g.selectAll(".legend")
-				.attr("transform", "translate(-100 -400), scale(1.85)");
-        	// set the opacity
-        	g.selectAll('.highest-emissions, .zero-emissions, .streets-top, .streets-bottom,.legend, .bg, .grid-cells')
-        	.attr("opacity", 0)
+		// queue()
+		// 	.defer(d3.json, "api/grid/")
+		// 	.await(addValidation)
 
-        }
+		// var validation = g.append("g")
+		// 	.attr("class", "validation")
 
-
-
-
+		// function addValidation(error, data){
+		// 	console.log(data);
 
 
+		// 	data.forEach(function(d) {
+		// 	    d.properties.co2_avg_e = +d.properties.co2_avg_e;
+		// 	    d.properties.tot_co2_e = +d.properties.tot_co2e;
+		// 	  });
 
+		// 	var x = d3.scale.log()
+		// 		.domain([1, 200])
+		// 	    .range([0.5, 500]);
+
+		// 	var y = d3.scale.log()
+		// 		.domain([1, 200])
+		// 	    .range([300, 0.5]);
+
+		// 	// Define the axes
+		// 	var xAxis = d3.svg.axis().scale(x)
+		// 	    .orient("bottom").ticks(5);
+
+		// 	var yAxis = d3.svg.axis().scale(y)
+		// 	    .orient("left").ticks(5);
+
+		// 	validation.selectAll(".dot")
+		// 	      .data(data)
+		// 	    .enter().append("circle")
+		// 	    .filter(function(d) { return d.properties.tot_co2e > 0 })
+		// 	      .attr("class", "dot")
+		// 	      .attr("r", 1.5)
+		// 	      .attr("cx", function(d) { return x(d.properties.co2_avg_e); })
+		// 	      .attr("cy", function(d) { return y(d.properties.tot_co2e); })
+		// 	      // .style("fill", function(d) { return color(d.properties.hoodgrouped); });
+
+		// 	// Add the X Axis
+		//     svg.append("g")
+		//         .attr("class", "x axis")
+		//         .attr("transform", "translate(50," + 450 + ")")
+		//         .call(xAxis);
+
+		//     // Add the Y Axis
+		//     svg.append("g")
+		//         .attr("class", "y axis")
+		//         .attr("transform", "translate(50, 50)")
+		//         .call(yAxis);
+
+		// }
+
+        // queue()
+        // 	.defer(d3.xml, "public/images/results.svg")
+        // 	.await(addResults)
+
+        // var results = g.append("g")
+        // 	.attr("class", "results")
+        // 	.attr("opacity", 0)
+        // 	.attr("transform", "scale(0.55)");
+
+        // function addResults(error, xml){
+        // 	console.log(xml);
+        // 	var items = Array.from(xml.getElementsByTagName("svg")[0].childNodes);
+        // 	items.forEach(function(val) {
+        // 	    results.node().appendChild(val);
+        // 	});
+
+        // }
+
+
+    //     queue()
+    //     	.defer(d3.xml, "public/images/emissions-map.svg")
+    //     	.await(addMap)
+
+    //     var emissionsMap = g.append("g")
+    //     	.attr("class", "emissionsMap");
+
+    // //     function addMap(error, xml){
+    // //     	console.log(xml);
+    // //     	var items = Array.from(xml.getElementsByTagName("svg")[0].childNodes);
+    // //     	items.forEach(function(val) {
+    // //     	    emissionsMap.node().appendChild(val);
+    // //     	});
+
+    // //     	g.selectAll(".emissionsMap")
+    // //     		.attr("transform", "translate(100), scale(0.5)");
+
+    // //     	g.selectAll(".legend")
+				// // .attr("transform", "translate(-100 -400), scale(1.85)");
+    // //     	// set the opacity
+    // //     	g.selectAll('.highest-emissions, .zero-emissions, .streets-top, .streets-bottom,.legend, .bg, .grid-cells')
+    // //     	.attr("opacity", 0)
+
+    // //     }
 
 	}; // end of setup vis
 
-
-	/**
-	 * ACTIVATE FUNCTIONS
-	 *
-	 * These will be called their
-	 * section is scrolled to.
-	 *
-	 * General pattern is to ensure
-	 * all content for the current section
-	 * is transitioned in, while hiding
-	 * the content for the previous section
-	 * as well as the next section (as the
-	 * user may be scrolling up or down).
-	 *
-	 */
-
-	/**
-	 * showTitle - initial title
-	 *
-	 * hides: count title
-	 * (no previous step to hide)
-	 * shows: intro title
-	 *
-	 */
 	function showTitle() {
 			// set the title text opacity to 1
 		g.selectAll(".openvis-title")
@@ -206,38 +256,26 @@ var scrollVis = function() {
 
 	}
 
-	function showHardware(){
-			// set the title text opacity to 1
-		g.selectAll(".openvis-title")
-			.transition()
-			.duration(600)
-			.attr("opacity", 0);
+	function showSensor(){
+		g.selectAll(".emissions")
+		    .attr("opacity", 0);
+		g.selectAll(".tower")
+		    .attr("opacity", 0);
+		g.selectAll(".car, .movement")
+		    .attr("opacity", 0);
+	    g.selectAll(".resistance")
+		    .attr("opacity", 0);
 
-		g.selectAll(".sensorsImg")
-		.attr("opacity", 1);
-
-			// add the current viz components
-			g.selectAll(".emissions")
-			    .transition()
-			    .duration(0)
-			    .attr("opacity", 0);
-			g.selectAll(".tower")
-			    .transition()
-			    .duration(0)
-			    .attr("opacity", 0);
-			g.selectAll(".car, .movement")
-			    .transition()
-			    .duration(0)
-			    .attr("opacity", 0);
-		    g.selectAll(".resistance")
-			    .transition()
-			    .duration(0)
-			    .attr("opacity", 0);
+		g.selectAll(".sensorAnnotated")
+			.attr("opacity", 1)
 
 	}
 
+
+
+
 	function showResistanceApproach(){
-		g.selectAll(".sensorsImg")
+		g.selectAll(".sensorAnnotated")
 			.attr("opacity", 0)
 
 		// add the current viz components
@@ -246,7 +284,6 @@ var scrollVis = function() {
 		    .duration(600)
 		    .delay(0)
 		    .attr("opacity", 1);
-
 		g.selectAll(".tower")
 		    .transition()
 		    .duration(600)
@@ -255,98 +292,172 @@ var scrollVis = function() {
 		g.selectAll(".car, .movement")
 		    .transition()
 		    .duration(600)
-		    .delay(1200)
+		    .delay(800)
 		    .attr("opacity", 1);
 	    g.selectAll(".resistance")
 		    .transition()
 		    .duration(600)
-		    .delay(1800)
+		    .delay(1200)
 		    .attr("opacity", 1);
 
-
-
-		// g.selectAll(".bike")
-		//     .transition()
-		//     .duration(600)
-		//     .delay(1500)
-		//     .attr("opacity", 1);
-
-		g.selectAll(".legend, .grid-cells")
-			.attr("opacity", 0)
-		g.selectAll(".highest-emissions")
-			.attr("opacity", 0)
-		g.selectAll(".zero-emissions")
-			.attr("opacity", 0)
-		// g.selectAll(".highlighter")
-		// 	.attr("opacity", 0)
-		g.selectAll(".streets-top,.streets-bottom,.bg")
+		g.selectAll(".results")
 			.attr("opacity", 0)
 
+			g.selectAll(".grid3d")
+				.attr("opacity", 0);
+	}
 
+
+	function show3dPlot(){
+			g.selectAll(".emissions")
+			    .attr("opacity", 0);
+			g.selectAll(".tower")
+			    .attr("opacity", 0);
+			g.selectAll(".car, .movement")
+			    .attr("opacity", 0);
+		    g.selectAll(".resistance")
+			    .attr("opacity", 0);
+
+
+			g.selectAll(".grid3d")
+				.transition()
+				.duration(600)
+				.attr("opacity", 1);
+
+
+			g.selectAll(".previewimg")
+			.attr("opacity", 0)
 
 	}
 
-	function showGridData(){
+	function showPreview(){
+		g.selectAll(".grid3d")
+				.attr("opacity", 0);
 
-		g.selectAll(".sensorsImg")
-			.attr("opacity", 0)
-
-		g.selectAll(".emissions")
-		    .transition()
-		    .duration(0)
-		    .attr("opacity", 0);
-		g.selectAll(".tower")
-		    .transition()
-		    .duration(0)
-		    .attr("opacity", 0);
-		g.selectAll(".car, .movement")
-		    .transition()
-		    .duration(0)
-		    .attr("opacity", 0);
-	    g.selectAll(".resistance")
-		    .transition()
-		    .duration(0)
-		    .attr("opacity", 0);
-
-
-
-		g.selectAll(".legend, .grid-cells")
-			.transition()
-			.duration(600)
+		g.selectAll(".previewimg")
 			.attr("opacity", 1)
-		g.selectAll(".streets-top,.streets-bottom,.bg")
-			.transition()
-			.duration(600)
-			.attr("opacity", 0.1)
-
-		g.selectAll(".highest-emissions")
-			.transition()
-			.duration(600)
-			// .delay(600)
-			.attr("opacity", 1)
-		g.selectAll(".zero-emissions")
-			.transition()
-			.duration(600)
-			// .delay(1200)
-			.attr("opacity", 1)
-
 	}
 
-	function showExploreData(){
+	// function showResults(){
+	// 	// add the current viz components
+	// 	g.selectAll(".emissions")
+	// 	    .attr("opacity", 0);
 
-		g.selectAll(".legend, .grid-cells")
-			.attr("opacity", 0)
-		g.selectAll(".highest-emissions")
-			.attr("opacity", 0)
-		g.selectAll(".zero-emissions")
-			.attr("opacity", 0)
-		g.selectAll(".streets-top,.streets-bottom,.bg")
-			.attr("opacity", 0)
+	// 	g.selectAll(".tower")
+	// 	    .attr("opacity", 0);
+	// 	g.selectAll(".car, .movement")
+	// 	    .attr("opacity", 0);
+	//     g.selectAll(".resistance")
+	// 	    .attr("opacity", 0);
+
+	// 	 g.selectAll(".grid3d")
+	// 			.attr("opacity", 0);
+
+	// 	g.selectAll(".results")
+	// 		.attr("opacity", 1)
+
+	// }
+
+
+	// function showGridData(){
+
+	// 	g.selectAll(".sensorsImg")
+	// 		.attr("opacity", 0)
+
+	// 	g.selectAll(".emissions")
+	// 	    .transition()
+	// 	    .duration(0)
+	// 	    .attr("opacity", 0);
+	// 	g.selectAll(".tower")
+	// 	    .transition()
+	// 	    .duration(0)
+	// 	    .attr("opacity", 0);
+	// 	g.selectAll(".car, .movement")
+	// 	    .transition()
+	// 	    .duration(0)
+	// 	    .attr("opacity", 0);
+	//     g.selectAll(".resistance")
+	// 	    .transition()
+	// 	    .duration(0)
+	// 	    .attr("opacity", 0);
+
+
+
+	// 	g.selectAll(".legend, .grid-cells")
+	// 		.transition()
+	// 		.duration(600)
+	// 		.attr("opacity", 1)
+	// 	g.selectAll(".streets-top,.streets-bottom,.bg")
+	// 		.transition()
+	// 		.duration(600)
+	// 		.attr("opacity", 0.1)
+
+	// 	g.selectAll(".highest-emissions")
+	// 		.transition()
+	// 		.duration(600)
+	// 		// .delay(600)
+	// 		.attr("opacity", 1)
+	// 	g.selectAll(".zero-emissions")
+	// 		.transition()
+	// 		.duration(600)
+	// 		// .delay(1200)
+	// 		.attr("opacity", 1)
+
+	// }
 
 
 
 
-	}
+	// function showExploreData(){
+
+	// 	g.selectAll(".legend, .grid-cells")
+	// 		.attr("opacity", 0)
+	// 	g.selectAll(".highest-emissions")
+	// 		.attr("opacity", 0)
+	// 	g.selectAll(".zero-emissions")
+	// 		.attr("opacity", 0)
+	// 	g.selectAll(".streets-top,.streets-bottom,.bg")
+	// 		.attr("opacity", 0)
+
+	// }
+
+	// function showTraces(){
+	// 	g.selectAll(".measure")
+	// 		.attr("opacity", 0);
+
+	// 	g.selectAll(".traces")
+	// 		.attr("opacity", 1);
+	// }
+
+	// function showHardware(){
+	// 		// set the title text opacity to 1
+	// 	g.selectAll(".openvis-title")
+	// 		.transition()
+	// 		.duration(600)
+	// 		.attr("opacity", 0);
+
+	// 	g.selectAll(".sensorsImg")
+	// 	.attr("opacity", 1);
+
+	// 		// add the current viz components
+	// 		g.selectAll(".emissions")
+	// 		    .transition()
+	// 		    .duration(0)
+	// 		    .attr("opacity", 0);
+	// 		g.selectAll(".tower")
+	// 		    .transition()
+	// 		    .duration(0)
+	// 		    .attr("opacity", 0);
+	// 		g.selectAll(".car, .movement")
+	// 		    .transition()
+	// 		    .duration(0)
+	// 		    .attr("opacity", 0);
+	// 	    g.selectAll(".resistance")
+	// 		    .transition()
+	// 		    .duration(0)
+	// 		    .attr("opacity", 0);
+
+	// }
 
 
 
