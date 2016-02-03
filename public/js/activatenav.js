833c6acad746a63a7d0a3494ad0f275ae05cbcc0
Joey Lee <josephkanglee@gmail.com>
2016-02-03 01:16:20 +0100

    rm aciivatejs

--- public/js/activatenav.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-// console.log($(".nav a"));
-// $(".nav a").on("click", function(){
-
-//    $(".nav a").find(".active").removeClass("active");
-//    $(this).parent().addClass("active");
-// });
-// })
-
